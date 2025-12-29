4e59ea0ff2292bd0b2cd4ba932c3ffb6211e7a67
Surbhi Meena <surbhimeena002@gmail.com>
2025-12-28 23:47:35 -0800

    Revert to GitHub Pages URL for updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Using custom domain www.surbhimeena.com
+  base: "/portfolio/", // Reverted to GitHub Pages URL for updates
 })
 
