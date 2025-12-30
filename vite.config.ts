753468637f8422a9c9fcb7e795c1ebbc5d503039
Surbhi Meena <surbhimeena002@gmail.com>
2025-12-29 18:46:10 -0800

    Set base path to /portfolio/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Base path for sam031195.github.io
+  base: "/portfolio/", // Base path for sam031195.github.io/portfolio/
 })
 
